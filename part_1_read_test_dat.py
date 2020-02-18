b99eb92404d1a8792628170634538d6f59dde76e
Rong Kang Chew <chewrongkang@gmail.com>
2020-02-18 09:43:19 -0500

    part 1 setup complete

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -6,3 +6,6 @@ input_dat_file = "data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+output = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+print(output)
